e31f21431d10ae5807758e6812d92029d8af5cae
Alex Zappa <alex@zappa.dev>
2025-12-17 02:07:32 +0100

    use build timestamp instead of hash for cache-busting and disable selector minification

--- postcss.config.js
+++ postcss.config.js
@@ -17,6 +17,7 @@ export default {
                 discardComments: {
                   removeAll: true,
                 },
+                minifySelectors: false,
               },
             ],
           }),
